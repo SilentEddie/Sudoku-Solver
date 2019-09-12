f09561ed14bbbf7c9d93f76d1de77d8a195a5e81
Edward Idema <edwardidema@gmail.com>
2019-09-12 19:28:17 +0200

    deleted some classes

--- src/main/java/com/novi/edward/sudokusolver/Main.java
+++ src/main/java/com/novi/edward/sudokusolver/Main.java
@@ -11,8 +11,23 @@ package com.novi.edward.sudokusolver;
  */
 public class Main {
     public static void main(String[] args) {
-        SudokuTester tester = new SudokuTester();
-        tester.start();
+                String PuzzleString =  "1.......6" 
+                + "..6.2.7.."
+                + "78945.1.3"
+                + "...8.7..4"
+                + "....3...."
+                + ".9...42.1"
+                + "31297..4."
+                + ".4..12.78"
+                + "9.8......";
+        if (PuzzleString.length() == 81) {
+            SudokuPuzzle testPuzzle = new SudokuPuzzle(PuzzleString);
+            System.out.println("Deze Puzzel gaat erin");
+            //System.out.println(testPuzzle.toString4());
+            System.out.println(testPuzzle.toString2());
+            System.out.println("Nu vragen we hem om te solven");
+            testPuzzle.solve();
+        }
+
     }
-    
 }
